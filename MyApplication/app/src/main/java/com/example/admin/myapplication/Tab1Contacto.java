a701e70ca8a75ccba3d01f8560e52a202fb43b45
TomasDamonte <tomas.damonte@comunidad.ub.edu.ar>
2017-09-23 19:40:36 -0300

    Agregados los .java de los tabs de la vista publica

---
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/Tab1Contacto.java
@@ -0,0 +1,20 @@
+package universis.universys;
+
+/**
+ * Created by Cecilia on 23/09/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Contacto extends Fragment{
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1contacto, container, false);
+        return rootView;
+    }
+}
