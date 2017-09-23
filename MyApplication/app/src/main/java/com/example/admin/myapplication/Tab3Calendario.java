034d8d511b121b154d8901728599c870b1ffb6b2
TomasDamonte <tomas.damonte@comunidad.ub.edu.ar>
2017-09-23 20:36:05 -0300

    agregado WebView a Tab3Calendario.java

--- MyApplication/app/src/main/java/com/example/admin/myapplication/Tab3Calendario.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/Tab3Calendario.java
@@ -1,20 +1,19 @@
 package universis.universys;
 
-/**
- * Created by Cecilia on 23/09/2017.
- */
-
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class Tab3Calendario extends Fragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab3calendario, container, false);
+        WebView webView=(WebView)rootView.findViewById(R.id.webCalendario);
+        webView.loadUrl("http://www.ub.edu.ar/academico.php?opcion=calendarios");
         return rootView;
     }
 }
