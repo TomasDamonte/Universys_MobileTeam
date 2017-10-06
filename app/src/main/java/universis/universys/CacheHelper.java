6232b3123f8e71901bdaded58492d7a6fef4f9b4
tomas <tomas.damonte@comunidad.ub.edu.ar>
2017-10-06 19:42:40 -0300

    Cambie las variables de cache

--- app/src/main/java/universis/universys/CacheHelper.java
+++ app/src/main/java/universis/universys/CacheHelper.java
@@ -9,14 +9,19 @@ import org.json.JSONObject;
 
 
 public class CacheHelper {
+
+    private static final String CACHE_CONFIGURATION_NAME = "cache";
+    private static final String CACHE_ERROR_NAME = "errorId";
+    private static final String CACHE_ERROR_NUMBER = "4516";
+
     public static Context context;
 
     public static String getStringProperty(Integer key) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("cache",Activity.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(CACHE_CONFIGURATION_NAME,Activity.MODE_PRIVATE);
         String res = null;
         if(!sharedPreferences.contains(key.toString())) {
             try {
-                return new JSONObject().put("errorId", "4516").toString();
+                return new JSONObject().put(CACHE_ERROR_NAME, CACHE_ERROR_NUMBER).toString();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -28,7 +33,7 @@ public class CacheHelper {
     }
 
     public static void setStringProperty(Integer key, String value) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("cache", Activity.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(CACHE_CONFIGURATION_NAME, Activity.MODE_PRIVATE);
         if (sharedPreferences != null) {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString(key.toString(), value);
