9c0ff2d8fd4da2eae03b98060217906a417bd233
tomas <tomas@damonte>
2017-10-07 01:33:18 -0300

    +TestDataBase.java

--- app/src/test/java/universis/universys/TestDataBase.java
+++ app/src/test/java/universis/universys/TestDataBase.java
@@ -2,7 +2,6 @@ package universis.universys;
 
 
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -11,9 +10,9 @@ import static org.junit.Assert.*;
 public class TestDataBase {
 
     @Test
-    public void TestRespuestaDB() throws JSONException {
+    public void testRespuestaDB() throws Exception {
         JSONObject expected = new JSONObject().put("errorId", "200").put("tipo", "alumno");
-        JSONObject test = new JSONObject().put("email", "pepito@mail").put("password", "pepitokpo91").put("tipo", "alumno");
+        JSONObject test = new JSONObject().put("mail", "alumno@alumno").put("password", "alumno");
         assertEquals(expected,DataBase.respuestaDB(test));
     }
 }
