3d7bee1f32b095bb56ed74adbbb52a927072982d
tomas <tomas@damonte>
2017-10-12 12:10:10 -0300

    +Clase URLs.java

---
+++ app/src/main/java/universis/universys/URLs.java
@@ -0,0 +1,7 @@
+package universis.universys;
+
+public class URLs {
+
+    public static final String LOGIN = "http://jsonplaceholder.typicode.com/posts";
+    public static final String DATOSALUMNO = "http://jsonplaceholder.typicode.com/posts";
+}
