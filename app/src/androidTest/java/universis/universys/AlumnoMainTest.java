c60209d421f85c9767931b95d329d4f9caa80ef0
tomas <tomas.damonte@comunidad.ub.edu.ar>
2017-11-02 20:36:02 -0300

    + AlumnoMainTest

---
+++ app/src/androidTest/java/universis/universys/AlumnoMainTest.java
@@ -0,0 +1,39 @@
+package universis.universys;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class AlumnoMainTest {
+
+    private AlumnoMain alumnoMain = new AlumnoMain();
+    private Context contexto = InstrumentationRegistry.getTargetContext();
+
+    @Test
+    public void enviarRequest() throws Exception {
+        
+    }
+
+    @Test
+    public void modificarDatosAlumno() throws Exception {
+
+    }
+
+    @Test
+    public void enviarDatosAlumno() throws Exception {
+
+    }
+
+    @Test
+    public void inscripcionesDisponibles() throws Exception {
+
+    }
+
+}
