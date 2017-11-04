8fac49e885ad698f350d2cb81e2abce4bcf33db7
tomas <tomas@damonte>
2017-11-04 01:47:35 -0300

    -AlumnoMainTest +ProfesorMainTest

--- app/src/androidTest/java/universis/universys/AlumnoMainTest.java
+++ app/src/androidTest/java/universis/universys/AlumnoMainTest.java
@@ -1,38 +1,52 @@
 package universis.universys;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import static org.junit.Assert.*;
 
-@RunWith(AndroidJUnit4.class)
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AlumnoMainTest {
 
-    private AlumnoMain alumnoMain = new AlumnoMain();
-    private Context contexto = InstrumentationRegistry.getTargetContext();
+    @Mock
+    AlumnoMain alumnoMain;
+    private View view;
+    private Context contexto;
+
+    public void setUp() throws Exception {
+        contexto = getInstrumentation().getContext();
+        assertNotNull(contexto);
+        view = new View(contexto);
+        assertNotNull(view);
+    }
 
     @Test
     public void enviarRequest() throws Exception {
-
+        alumnoMain.enviarRequest(null);
+        alumnoMain.enviarRequest(view);
     }
 
     @Test
     public void modificarDatosAlumno() throws Exception {
-
+        alumnoMain.modificarDatosAlumno(null);
+        alumnoMain.modificarDatosAlumno(view);
     }
 
     @Test
     public void enviarDatosAlumno() throws Exception {
-
+        alumnoMain.enviarDatosAlumno(null);
+        alumnoMain.enviarDatosAlumno(view);
     }
 
     @Test
     public void inscripcionesDisponibles() throws Exception {
-
+        alumnoMain.inscripcionesDisponibles();
     }
 
 }
--- app/src/main/java/universis/universys/ProfesorMain.java
+++ app/src/main/java/universis/universys/ProfesorMain.java
@@ -1,25 +1,17 @@
 package universis.universys;
 
-import android.annotation.TargetApi;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.print.PrintAttributes;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
