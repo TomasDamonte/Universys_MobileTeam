c062ca40621cf6c73427ac878271943cab61b768
tomas <tomas@damonte>
2017-11-05 13:03:12 -0300

    Comenté la clase JSONBuilder

--- app/src/main/java/universis/universys/JSONBuilder.java
+++ app/src/main/java/universis/universys/JSONBuilder.java
@@ -1,20 +1,23 @@
 package universis.universys;
 
 
-import android.util.Log;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+/**
+ * Esta clase construye los JSON que se envían en las request al servidor.
+ */
 public class JSONBuilder {
 
+    //Versión actual de la API.
     public static final String APIVER = "1.0";
     private LinkedHashMap<String,String> datos;
 
+    /**
+     * Constructor de la clase.
+     */
     public JSONBuilder() {
         datos = new LinkedHashMap<>();
         datos.put("apiVer",APIVER);
@@ -24,16 +27,33 @@ public class JSONBuilder {
             datos.put("idSesion","000");
     }
 
+    /**
+     * Constructor que se utiliza en el login.
+     * @param email String con el email del usuario.
+     * @param password String con la contraseña del usuario.
+     * @return JSONObject a eviar en el body de la request.
+     */
     public JSONObject logIn(String email, String password) {
         datos.put("mail",email);
         datos.put("password",password);
         return new JSONObject(datos);
     }
 
+    /**
+     * Devuelve el contenido del constructor de la clase.
+     * @return JSONObject a eviar en el body de la request.
+     */
     public JSONObject requestBasico() {
         return new JSONObject(datos);
     }
 
+    /**
+     * Constructor usado cuando se necesita enviar cátedra,carrera y materia.
+     * @param catedra Catedra a enviar.
+     * @param carrera Carrera a enviar.
+     * @param materia Materia a enviar.
+     * @return JSONObject a eviar en el body de la request.
+     */
     public JSONObject requestGenerico(String catedra, String carrera, String materia) {
         datos.put("catedra",catedra);
         datos.put("carrera",carrera);
@@ -41,6 +61,15 @@ public class JSONBuilder {
         return new JSONObject(datos);
     }
 
+    /**
+     * Constructor usado cuando se necesita enviar cátedra,carrera, materia, alumno y nota.
+     * @param catedra Catedra a enviar.
+     * @param carrera Carrera a enviar.
+     * @param materia Materia a enviar.
+     * @param alumno Alumno a enviar.
+     * @param nota Nota a enviar.
+     * @return JSONObject a eviar en el body de la request.
+     */
     public JSONObject requestGenerico(String catedra, String carrera, String materia, String alumno, String nota) {
         datos.put("catedra",catedra);
         datos.put("carrera",carrera);
@@ -50,6 +79,11 @@ public class JSONBuilder {
         return new JSONObject(datos);
     }
 
+    /**
+     * Constructor usado cuando el profesor acepta/rechaza solicitudes de inscripcion de alumnos a sus materias.
+     * @param solicitudes LinkedHashMap con el id de solicitud y el estado.(idSolicitud,estado) Estado=>"aceptada"/"rechazada"
+     * @return JSONObject a eviar en el body de la request.
+     */
     public JSONObject enviarSolicitudesInscripcion(LinkedHashMap<String,String> solicitudes) {
         JSONObject body = new JSONObject(datos);
         try {
@@ -60,6 +94,11 @@ public class JSONBuilder {
         return body;
     }
 
+    /**
+     * Constructor usado cuando se envían los datos personales del usuario.
+     * @param datosPersonales Vector de String que contiene el valor de cada campo.
+     * @return JSONObject a eviar en el body de la request.
+     */
     public  JSONObject modificarDatosPersonales(String[] datosPersonales) {
         datos.put("nombre",datosPersonales[0]);
         datos.put("apellido",datosPersonales[1]);
@@ -70,6 +109,11 @@ public class JSONBuilder {
         return  new JSONObject(datos);
     }
 
+    /**
+     * Constructor usado cuando se envian los datos de la inscripción a una materia de un alumno.
+     * @param idCursada id de la cuarsada a la cual el alumno desea inscribirse.
+     * @return JSONObject a eviar en el body de la request.
+     */
     public JSONObject inscripcionAMateria(String idCursada) {
         datos.put("idCursada",idCursada);
         return new JSONObject(datos);
